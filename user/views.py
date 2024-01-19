0f51f1024458824e3a6a366e1c61c6c34a3f97e3
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-01-19 07:29:26 +0600

    link

--- user/views.py
+++ user/views.py
@@ -23,7 +23,7 @@ def registration(request):
 
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link = f"http://127.0.0.1:8000/user/active/{uid}/{token}"
+            confirm_link = f"https://trainease.onrender.com/user/active/{uid}/{token}"
             email_subject = "Confirm Your Email"
             email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
             email = EmailMultiAlternatives(email_subject , '', to=[user.email])
