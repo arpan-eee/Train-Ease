8bd458105b62f331082c789f019550bb05597f50
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-01-26 21:04:15 +0600

    imagefield

--- train/models.py
+++ train/models.py
@@ -43,6 +43,7 @@ class Train(models.Model):
     to_station = models.ForeignKey(Station, blank=True, null=True, on_delete=models.CASCADE,related_name='to_station')
     departure = models.TimeField()
     ticket_price = models.DecimalField(max_digits=10, decimal_places=2 , blank=True, null=True)
+    image = models.ImageField(upload_to='train/media/uploads/', blank = True, null = True)
 
     def __str__(self):
         return self.name
